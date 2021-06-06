623be81cc3f6bafd23419793e927ee87cb86b03b
Yuichi Ishibashi <issyu039@gmail.com>
2021-06-06 13:52:55 +0900

    abc203のC問題を追加

--- AtCoder/abc203/abc203_c.cpp
+++ AtCoder/abc203/abc203_c.cpp
@@ -4,34 +4,51 @@
 #include <vector>  
 using namespace std;
 
-struct friend {
-  int index;  // インデックス
-  int amount; // 渡す金額
-};
-
-bool index_cmp(const struct friend& p, const struct friend& q) {
-    return p.index > q.index;
-}
-
 int main() {
     long long N, K;
+    long long ans;
+    vector<pair<long long, long long> > friends;
 
     // 入力を変数に格納
     cin >> N >> K;
-
-    vector<friend> friends(N);
-    long long index;
-    long long amount;
-
-    // 入力を変数に格納
     for (int i=0; i<N; i++) {
-		cin >> index >> amount;
-        village v = {index, amount};
-        villages.push_back(v);
+        long long index;
+        long long amount;
+        cin >> index >> amount;
+        friends.push_back(make_pair(index, amount));
 	}
+    
+    // 村番号が小さい順にソート
+    sort(friends.begin(), friends.end());
 
-    sort(friends.begin(), friends.end(), index_cmp);
+    // 各友達について計算する
+    for (int i=0; i<N; i++) {
+        long long dis;
+        // i人目まで来れているかを確認する
+        if (i==0) {
+            dis = friends[i].first;
+            if (K < dis) {
+                ans = K;
+                break;
+            } 
+        } else {
+            dis = friends[i].first - friends[i-1].first;
+            if (K < dis) {
+                ans = friends[i-1].first + K;  
+                break;
+            } 
+        }
 
-    // TODO: 各友達について計算する
-  
+        // 所持金更新
+        K = K - dis;
+        // 来れていたら所持金に金額を足す
+        K = K + friends[i].second;
+
+        // 最後の友達からお金をもらったら、可能な限り行ける村番号を答えとする
+        if (i==N-1) {
+            ans = friends[i].first + K;
+        }
+    }  
+
+    cout << ans << endl; 
 }
